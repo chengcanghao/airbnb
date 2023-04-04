ea38e103961aab7a8248009493295153b18578f4
itheima <chengcanghao@outlook.com>
2023-04-04 12:23:39 +0800

    u

---
+++ craco.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+const resolve = pathname => path.resolve(__dirname, pathname)
+
+module.exports = {
+  // less
+ 
+  // webpack
+  webpack: {
+    alias: {
+      "@": resolve("src"),
+      "components": resolve("src/components"),
+      "utilities": resolve("src/utilities")
+    }
+  }
+}
