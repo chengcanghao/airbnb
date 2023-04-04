8d7ffec729ecbf707aaa28e2773da2e609b54690
chengcanghao <chengcanghao@outlook.com>
2023-04-04 22:45:07 +0800

    u

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,28 +1,10 @@
 import React, { memo, useEffect, useState } from 'react'
 import CCHRequest from '@/services'
 const Home = memo(() => {
-  const [highScore, setHighScore] = useState({})
-
-  useEffect(() => {
-
-    CCHRequest.get({ url: '/home/highscore' }).then(res => {
-      console.log(res)
-      setHighScore(res)
-    })
-
-  }, [])
-
+  
   return (
-    <div>Home
-      <div className='highScore'>
-        <ul>
-          {
-            highScore?.list?.map(element => {
-              return <li key={element.id}>{element.name}</li>
-            })
-          }
-        </ul>
-      </div>
+    <div>
+      Home
     </div>
   )
 })
